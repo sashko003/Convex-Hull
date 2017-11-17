ddf33b9bf0923bb946534d99387af99e3f333a2b
Sasha <31618118+sashko003@users.noreply.github.com>
2017-11-17 11:52:51 +0200

    Create header.h
    All files and libraries I use.

---
+++ header.h
@@ -0,0 +1,15 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+#include <iostream>
+using std::cout;
+#include <fstream>
+#include <list>
+using std::list;
+#include <cmath>
+#include<algorithm>
+
+#include "point.h"
+#include "shell.h"
+
+#endif // HEADER_H
